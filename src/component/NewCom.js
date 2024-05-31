ba5cd02eb69d7d2cec556ff4721f30b72cb3b716
RahulRSoni <114871652+RahulRSoni@users.noreply.github.com>
2024-05-31 18:11:42 +0530

    Added CORS

--- src/component/NewCom.js
+++ src/component/NewCom.js
@@ -1,117 +1,128 @@
-import React, { useEffect, useState } from 'react'
-import NewItem from './NewItem'
+import React, { useEffect, useState } from 'react';
+import NewItem from './NewItem';
 import Spinner from './Spinner';
-import PropTypes from 'prop-types'
-import InfiniteScroll from "react-infinite-scroll-component";
-
-
+import PropTypes from 'prop-types';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import cors from 'cors';
 
 const NewCom = (props) => {
+	const [articles, setArticles] = useState([]);
+	const [loading, setLoaing] = useState(true);
+	const [page, setPage] = useState(1);
+	const [totalResults, setTotalResults] = useState(0);
 
-  const [articles, setArticles] = useState([])
-  const [loading, setLoaing] = useState(true)
-  const [page, setPage] = useState(1)
-  const [totalResults, setTotalResults] = useState(0);
-  
-  const capitalizeFirstLetter = (string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
-  
-  const updateNews = async () => {
-    props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pagesize=${props.pageSize}`;
-    setLoaing(true);
-    let data = await fetch(url);
-    props.setProgress(30);
-    let parseData = await data.json();
-    props.setProgress(60);
-    
-    setArticles(parseData.articles);
-    setPage(page);
-    setTotalResults(parseData.totalResults);
-    setLoaing (false);
-    
-    props.setProgress(100);
-  }
+	const capitalizeFirstLetter = (string) => {
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	};
 
-  useEffect(() => {
-    document.title = `${capitalizeFirstLetter(props.category)} | NewsSpot`;
+	const updateNews = async () => {
+		props.setProgress(10);
+		const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pagesize=${props.pageSize}`;
+		setLoaing(true);
+		let data = await fetch(url, cors());
+		props.setProgress(30);
+		let parseData = await data.json();
+		props.setProgress(60);
 
-    updateNews();
-    // eslint-disable-next-line
-  }, [])
+		setArticles(parseData.articles);
+		setPage(page);
+		setTotalResults(parseData.totalResults);
+		setLoaing(false);
 
-  const fetchMoreData = async () => {
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pagesize=${props.pageSize}`;
-    setPage(page + 1);
-    let data = await fetch(url);
-    let parseData = await data.json();
-    setArticles(articles.concat(parseData.articles));
-    setTotalResults(parseData.totalResults);
-    }
-  
+		props.setProgress(100);
+	};
 
-  // hendelPreClick = async () => {
-  //   this.setState({
-  //     page: this.state.page - 1
-  //   })
-  //   this.updateNews()
-  // }
+	useEffect(() => {
+		document.title = `${capitalizeFirstLetter(props.category)} | NewsSpot`;
 
-  // hendelNextClick = async () => {
-  //   this.setState({
-  //     page: this.state.page + 1
-  //   })
-  //   this.updateNews()
-  // }
+		updateNews();
+		// eslint-disable-next-line
+	}, []);
 
-  
-    return (
-      <>
-        <div className="text-center">
-          <h2>Top Headlines</h2>
-          {loading && <Spinner />}
-        </div>
+	const fetchMoreData = async () => {
+		const url = `https://newsapi.org/v2/top-headlines?country=${
+			props.country
+		}&category=${props.category}&apiKey=${props.apiKey}&page=${
+			page + 1
+		}&pagesize=${props.pageSize}`;
+		setPage(page + 1);
+		let data = await fetch(url);
+		let parseData = await data.json();
+		setArticles(articles.concat(parseData.articles));
+		setTotalResults(parseData.totalResults);
+	};
 
-        <InfiniteScroll
-          dataLength={articles.length}
-          next={fetchMoreData}
-          hasMore={articles.length !== totalResults}
-          loader={<Spinner />}
-        >
-          <div className="container my-3">
-            <div className="row my-4 d-flex justify-content-evenly" >
-              {articles.map((element, index) => {
-                return <div className="col-md-4 mx-36" key={index}>
-                  <NewItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 80) : ""} imgUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
-                </div>
-              })}
-            </div>
-          </div>
+	// hendelPreClick = async () => {
+	//   this.setState({
+	//     page: this.state.page - 1
+	//   })
+	//   this.updateNews()
+	// }
 
-        </InfiniteScroll>
+	// hendelNextClick = async () => {
+	//   this.setState({
+	//     page: this.state.page + 1
+	//   })
+	//   this.updateNews()
+	// }
 
-        {/* <div className='container d-flex justify-content-around my-5'>
+	return (
+		<>
+			<div className='text-center'>
+				<h2>Top Headlines</h2>
+				{loading && <Spinner />}
+			</div>
+
+			<InfiniteScroll
+				dataLength={articles.length}
+				next={fetchMoreData}
+				hasMore={articles.length !== totalResults}
+				loader={<Spinner />}>
+				<div className='container my-3'>
+					<div className='row my-4 d-flex justify-content-evenly'>
+						{articles.map((element, index) => {
+							return (
+								<div
+									className='col-md-4 mx-36'
+									key={index}>
+									<NewItem
+										title={element.title ? element.title.slice(0, 45) : ''}
+										description={
+											element.description
+												? element.description.slice(0, 80)
+												: ''
+										}
+										imgUrl={element.urlToImage}
+										newsUrl={element.url}
+										author={element.author}
+										date={element.publishedAt}
+										source={element.source.name}
+									/>
+								</div>
+							);
+						})}
+					</div>
+				</div>
+			</InfiniteScroll>
+
+			{/* <div className='container d-flex justify-content-around my-5'>
           <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.hendelPreClick}>&larr; Previous</button>
           <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / props.pageSize)} type="button" className="btn btn-dark" onClick={this.hendelNextClick}>Next &rarr;</button>
         </div> */}
-
-      </>
-    )
-  }
-
-
+		</>
+	);
+};
 
 NewCom.defaultProps = {
-  country: "in",
-  pageSize: 21,
-  category: "general"
-}
+	country: 'in',
+	pageSize: 21,
+	category: 'general',
+};
 
 NewCom.propTypes = {
-  country: PropTypes.string,
-  pageSize: PropTypes.number,
-  category: PropTypes.string
-}
+	country: PropTypes.string,
+	pageSize: PropTypes.number,
+	category: PropTypes.string,
+};
 
 export default NewCom;
